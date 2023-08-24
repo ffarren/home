21974bde2e83e0cecdea93ef60a32214260d8016
ffarren <farrenfidelis@gmail.com>
2023-08-24 20:08:35 +0700

    add responsive design

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import "./card.css"
 
 const Card = (prop) => {
   return (
-    <div className="hover:cursor-pointer mx-96 my-6 pressedEffect bg-white ">
+    <div className="hover:cursor-pointer mx-20 md:mx-40 lg:mx-60 my-6 pressedEffect bg-white ">
       <a href={prop.link} target="_blank" rel="noreferrer">
         <p className="font-mono bg-white hover:bg-slate-200 rounded-2xl p-4 border-2 border-black">{prop.text}</p>
       </a>
