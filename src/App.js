6ac875d17a33095c712ffff8e2dca7ab90f67977
ffarren <farrenfidelis@gmail.com>
2023-08-24 11:55:38 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Card from "./Card";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center">
+      <div className="mt-20">
+        <img
+          alt="profile"
+          className="rounded-full block mx-auto"
+          width="130"
+          height="130"
+          src="./media/profile.jpg"
+        ></img>
+      </div>
+      <h1 className="mt-10 font-mono text-xl text-white">
+        Fidelis Farren Pradipto
+      </h1>
+      <Card
+        text="Lihat CV"
+        link="https://drive.google.com/file/d/1GRCiQBkwBqTOl9WgSqv-tiT7RAOEBsdx/view?usp=sharing"
+      />
+      <Card
+        text="Farren Space"
+        link="https://ffarren.github.io/farren-space/"
+      />
+      <Card text="Email" link="mailto:fidelisfarren.work@gmail.com" />
+      <Card text="Instagram" link="https://www.instagram.com/ffarrenp/" />
+      <Card text="VSCO" link="https://vsco.co/ffarren/gallery" />
+
+      <footer className="p-10">
+        <p className="font-mono text-white">made by farren</p>
+      </footer>
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import "./card.css"
+
+const Card = (prop) => {
+  return (
+    <div className="hover:cursor-pointer mx-96 my-6 pressedEffect bg-white ">
+      <a href={prop.link} target="_blank" rel="noreferrer">
+        <p className="font-mono bg-white hover:bg-slate-200 rounded-2xl p-4 border-2 border-black">{prop.text}</p>
+      </a>
+    </div>
+  );
+};
+
+export default Card;
